73d2d0c4a105d9a70ac8bc3dcc54d7c29a4e6d88
Justin Ho Lung <justin.holung@gmail.com>
2021-03-23 23:26:57 -0500

    Final Project commit

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://proj:masterjdh21@localhost/proj'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://hlkklpmzgftqqz:c32b9ab55d84ede6fb5c5ff966961746b3075b1060a2952c1ebc4e7ac70ecb99@ec2-52-71-161-140.compute-1.amazonaws.com:5432/dcmeumrci0qsmu'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './uploads'
 
